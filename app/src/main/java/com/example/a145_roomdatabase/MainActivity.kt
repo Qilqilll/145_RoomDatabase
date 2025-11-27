fac7b575a9485cc00296f493bd55fa522673a625
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-27 21:33:52 +0700

    edit halaman dan home view model lalu ditampilkan lewat mainactifity

--- app/src/main/java/com/example/a145_roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/a145_roomdatabase/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.a145_roomdatabase
+package com.example.a145_RoomDatabase
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,41 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.a145_roomdatabase.ui.theme._145_RoomDatabaseTheme
+import com.example.a145_RoomDatabase.ui.theme.A145_RoomDatabaseTheme
+import com.example.a145_RoomDatabase.view.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _145_RoomDatabaseTheme {
+            a145_RoomDatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    SiswaApp(
+                        modifier = Modifier.padding(paddingValues = innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _145_RoomDatabaseTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/a145_roomdatabase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/a145_roomdatabase/viewmodel/HomeViewModel.kt
@@ -6,27 +6,26 @@ import com.example.a145_roomdatabase.repositori.RepositoriSiswa
 import com.example.a145_roomdatabase.room.Siswa
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
-class HomeViewModel(
-    private val repositoriSiswa: RepositoriSiswa
-) : ViewModel() {
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
 
-    val homeUiState: StateFlow<HomeUiState> =
-        repositoriSiswa.getAllSiswaStream()
-            .map { HomeUiState(listSiswa = it.toList()) }   // gunakan nama yg benar
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = TIMEOUT_MILLIS),
-                initialValue = HomeUiState()
-            )
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
 
     data class HomeUiState(
-        val listSiswa: List<Siswa> = listOf()   // perbaikan listOf()
+        val listSiswa: List<Siswa> = listOf()
     )
 }
