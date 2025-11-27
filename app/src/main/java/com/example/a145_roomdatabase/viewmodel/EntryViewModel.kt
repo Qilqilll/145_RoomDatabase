90eaf5abd702753729517b7eb3249774827203e8
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-27 17:09:44 +0700

    import

--- app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,4 +1,8 @@
 package com.example.a145_roomdatabase.viewmodel
 
-import org.intellij.lang.annotations.Identifier
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.a145_roomdatabase.repositori.RepositoriSiswa
+import com.example.a145_roomdatabase.room.Siswa
