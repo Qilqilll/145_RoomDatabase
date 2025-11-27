e420b6368f35971f44f14da5d99a17fe6ef67355
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-27 16:33:48 +0700

    offlinerepositor

--- app/src/main/java/com/example/a145_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a145_roomdatabase/repositori/ContainerApp.kt
@@ -7,3 +7,10 @@ import com.example.a145_roomdatabase.room.DatabaseSiswa
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
