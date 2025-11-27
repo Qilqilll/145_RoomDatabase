2b5f5d4ee377b5931221ff827c0e8e4f893da086
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-27 17:10:19 +0700

    nama alamat dan telpon tidak boleh kosong

--- app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
@@ -6,3 +6,16 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.a145_roomdatabase.repositori.RepositoriSiswa
 import com.example.a145_roomdatabase.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+}
