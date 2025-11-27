a4137925394fa0736247035c5591f37ec40c3ab6
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-27 21:33:20 +0700

    entry dan halaman entry

--- app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,3 +1,4 @@
+// kotlin
 package com.example.a145_roomdatabase.viewmodel
 
 import androidx.compose.runtime.getValue
@@ -9,23 +10,35 @@ import com.example.a145_roomdatabase.room.Siswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
 
+    /**
+     * Berisi status Siswa saat ini
+     */
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-
+    /* Fungsi untuk memvalidasi input */
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
-}
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
-
 
 data class DetailSiswa(
     val id: Int = 0,
@@ -34,7 +47,6 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
-
 fun DetailSiswa.toSiswa(): Siswa = Siswa(
     id = id,
     nama = nama,
@@ -42,13 +54,11 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     telpon = telpon
 )
 
-
 fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
     UIStateSiswa(
         detailSiswa = this.toDetailSiswa(),
         isEntryValid = isEntryValid
     )
-
 
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
