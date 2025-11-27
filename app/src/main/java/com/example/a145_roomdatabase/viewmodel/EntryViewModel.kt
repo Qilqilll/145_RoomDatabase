4417e6012ba6ebb0dbe85d068ff0c749c7c8b9e1
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-27 17:11:38 +0700

    fun siswa ui

--- app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a145_roomdatabase/viewmodel/EntryViewModel.kt
@@ -34,3 +34,18 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
+    UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
