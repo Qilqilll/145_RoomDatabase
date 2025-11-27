063379c1006f2a8196cf118646b1ef917a4c5d68
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-27 16:21:31 +0700

    import repositoriSiswa.kt

---
+++ app/src/main/java/com/example/a145_roomdatabase/repositori/repositoriSiswa.kt
@@ -0,0 +1,6 @@
+package com.example.a145_roomdatabase.repositori
+
+import com.example.a145_roomdatabase.room.Siswa
+import com.example.a145_roomdatabase.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
