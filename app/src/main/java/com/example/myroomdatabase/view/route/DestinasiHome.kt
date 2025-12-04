8f7c762b5172f1934e8b449bddfb83b28f1d6703
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-12-04 21:35:40 +0700

    tampilan objek home

---
+++ app/src/main/java/com/example/myroomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.myroomdatabase.view.route
+
+import com.example.myroomdatabase.R
+import com.example.myroomsatu.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
