f4776cc9ae7d6971576c03660532c8917930b6cf
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-12-04 21:35:29 +0700

    destinasi entry

---
+++ app/src/main/java/com/example/myroomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.myroomdatabase.view.route
+
+import com.example.myroomdatabase.R
+import com.example.myroomsatu.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
