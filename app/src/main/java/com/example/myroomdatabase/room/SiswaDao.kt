4e402ed549fbf333df6678c0b415fa86378ee0a1
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-12-04 21:37:31 +0700

    siswa

---
+++ app/src/main/java/com/example/myroomdatabase/room/SiswaDao.kt
@@ -0,0 +1,29 @@
+package com.example.myroomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+    // --- Bagian Baru untuk Fitur Edit & Detail ---
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+}
